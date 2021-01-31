3dc4e87b311b87bb8d7810f5240bd49eef5e088f
hsgw <urkwtky@gmail.com>
2021-01-31 16:28:11 +0900

    implement keycode.ts, test is not enough

--- test/keycode.test.ts
+++ test/keycode.test.ts
@@ -1,10 +1,32 @@
-import { KeycodeList } from '~/utils/keycodeList'
+import { KeycodeBuilder } from '~/utils/keycode'
 
 describe('keycode', () => {
-  test('simple RAW HID code (4)', () => {
-    expect(KeycodeList.FindByRAW(4)?.qmk).toBe('KC_A')
+  test('simple (4)', () => {
+    const keycode = KeycodeBuilder.BuildFromRAW(4)
+    expect(keycode).toEqual({
+      type: 'BASIC',
+      qmk: 'KC_A',
+      raw: 4,
+      mods: [],
+      base: expect.anything(),
+    })
   })
-  test('build from simple QMK name (KC_A)', () => {
-    expect(KeycodeList.FindByQMK('KC_A')?.raw).toBe(4)
+  test('with mod (S(KC_A))', () => {
+    expect(KeycodeBuilder.BuildFromRAW(4 + 0x0200)).toEqual({
+      type: 'BASIC',
+      qmk: 'LSFT(KC_A)',
+      raw: 4 + 0x0200,
+      mods: ['LSFT'],
+      base: expect.anything(),
+    })
+  })
+  test('with mod (RGUI(RCTL(KC_A)))', () => {
+    expect(KeycodeBuilder.BuildFromRAW(4 + 0x1900)).toEqual({
+      type: 'BASIC',
+      qmk: 'RGUI(RCTL(KC_A))',
+      raw: 4 + 0x1900,
+      mods: ['RCTL', 'RGUI'],
+      base: expect.anything(),
+    })
   })
 })
