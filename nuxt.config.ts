27dd0a34c0031c43af46b1fb18ef4912ba78b41d
hsgw <urkwtky@gmail.com>
2021-03-02 15:23:21 +0900

    add ogp meta tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,12 +9,44 @@ const config: NuxtConfig = {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - loom',
-    title: 'loom',
+    htmlAttrs: {
+      lang: 'us',
+      prefix: 'og: http://ogp.me/ns#',
+    },
+    titleTemplate: 'LOOM',
+    title: 'LOOM',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'LOOM Configulator is a keymap editor for keyboards made with QMK Firmware.',
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'LOOM Configulator',
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://loom-conf.github.io/',
+      },
+      { hid: 'og:title', property: 'og:title', content: 'Loom Configulator' },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'LOOM Configulator is a keymap editor for keyboards made with QMK Firmware.',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://loom-conf.github.io/ogp.png',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
